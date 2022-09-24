37984d4eba12a30cb01caeaba82942ad203c331f
Ahmet Furkan Güneş <111661927+ahmetfurkangunes@users.noreply.github.com>
2022-09-24 17:32:08 +0300

    Add files via upload

---
+++ Bankapplication/Bankapp/src/Bank.java
@@ -0,0 +1,10 @@
+public class Bank {
+    public static void main(String[] args) {
+        Account ahmet = new Account("Ahmet Furkan Güneş", "A00001");
+        ahmet.showMenu();
+
+        
+        
+    }
+    
+}
